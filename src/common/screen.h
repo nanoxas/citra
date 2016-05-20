c619b4ee4f385590593ef459bf97a31a5153a45b
James Rowe <jroweboy@gmail.com>
2016-05-19 19:07:09 -0600

    Inital work

---
+++ src/common/screen.h
@@ -0,0 +1,13 @@
+// Copyright 2016 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+class Screen {
+public:
+    Screen();
+    ~Screen();
+private:
+
+};
