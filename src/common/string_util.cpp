8b1e7ccf6af739879b152c690f1bc6bf167f080a
yami-hack <yami-hack@foxmail.com>
2016-06-11 16:17:33 +0800

    Add "MinGW" precompile Signed-off-by: yami-hack <yami-hack@foxmail.com>
    Remove unnecessary code,using secure API
    Signed-off-by: yami-hack <yami-hack@foxmail.com>

--- src/common/string_util.cpp
+++ src/common/string_util.cpp
@@ -13,7 +13,7 @@
 #include "common/logging/log.h"
 #include "common/string_util.h"
 
-#ifdef _MSC_VER
+#if defined(_MSC_VER) || defined(__MINGW64__)
     #include <Windows.h>
     #include <codecvt>
     #include "common/common_funcs.h"
@@ -292,7 +292,7 @@ std::string ReplaceAll(std::string result, const std::string& src, const std::st
     return result;
 }
 
-#ifdef _MSC_VER
+#if defined(_MSC_VER) || defined(__MINGW64__)
 
 std::string UTF16ToUTF8(const std::u16string& input)
 {
