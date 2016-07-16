4237317774fbe7e3c35dfb3064b532a148f531e5
James Rowe <jroweboy@gmail.com>
2016-07-16 10:36:25 -0600

    Use iconv instead of codecvt which isnt supported in gcc 4.9

--- src/common/string_util.cpp
+++ src/common/string_util.cpp
@@ -13,7 +13,7 @@
 #include "common/logging/log.h"
 #include "common/string_util.h"
 
-#if defined(_MSC_VER) || defined(__MINGW64__)
+#if defined(_MSC_VER) || (defined(__MINGW64__) && __GNUC__ > 4)
     #include <windows.h>
     #include <codecvt>
     #include "common/common_funcs.h"
