3106bfbe469e4833f3621df892744f63836bb65b
James Rowe <jroweboy@gmail.com>
2018-01-23 23:14:07 -0700

    Remove unused code

--- src/video_core/video_core.h
+++ src/video_core/video_core.h
@@ -22,7 +22,6 @@ extern EmuWindow* g_emu_window;                  ///< Emu window
 // qt ui)
 extern std::atomic<bool> g_hw_renderer_enabled;
 extern std::atomic<bool> g_shader_jit_enabled;
-extern std::atomic<u16> g_frame_limit;
 
 /// Start the video core
 void Start();
@@ -33,4 +32,4 @@ bool Init(EmuWindow* emu_window);
 /// Shutdown the video core
 void Shutdown();
 
-} // namespace
+} // namespace VideoCore
