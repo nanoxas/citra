3b17d4e8c4b7efdcd20c0d57e0f5fd3c04868d2e
BreadFish64 <mohror64@gmail.com>
2018-09-06 19:38:54 -0500

    weird dynarmic hack

--- src/core/arm/dynarmic/arm_dynarmic.cpp
+++ src/core/arm/dynarmic/arm_dynarmic.cpp
@@ -276,6 +276,6 @@ std::unique_ptr<Dynarmic::A32::Jit> ARM_Dynarmic::MakeJit() {
     config.callbacks = cb.get();
     config.page_table = &current_page_table->pointers;
     config.coprocessors[15] = std::make_shared<DynarmicCP15>(interpreter_state);
-    config.define_unpredictable_behaviour = true;
+    //config.define_unpredictable_behaviour = true;
     return std::make_unique<Dynarmic::A32::Jit>(config);
 }
