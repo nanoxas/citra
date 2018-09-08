5b99e65b39dd21bfc998f9c67ce364ff110acab5
BreadFish64 <mohror64@gmail.com>
2018-09-07 22:35:13 -0500

    open file [fails]

--- src/android/app/src/main/cpp/native-lib.cpp
+++ src/android/app/src/main/cpp/native-lib.cpp
@@ -1,3 +1,44 @@
 #include <jni.h>
 #include <string>
+#include <memory>
+#include <utility>
+#include <string>
+#include "core/frontend/emu_window.h"
+#include "core/core.h"
 
+class EmuWindow_Android : public EmuWindow {
+public:
+    explicit EmuWindow_Android();
+    ~EmuWindow_Android();
+
+    /// Swap buffers to display the next frame
+    void SwapBuffers() override;
+
+    /// Polls window events
+    void PollEvents() override;
+
+    /// Makes the graphics context current for the caller thread
+    void MakeCurrent() override;
+
+    /// Releases the GL context from the caller thread
+    void DoneCurrent() override;
+
+private:
+};
+
+EmuWindow_Android::EmuWindow_Android() = default;
+EmuWindow_Android::~EmuWindow_Android() = default;
+void EmuWindow_Android::SwapBuffers() {}
+void EmuWindow_Android::PollEvents(){}
+void EmuWindow_Android::MakeCurrent(){}
+void EmuWindow_Android::DoneCurrent(){}
+
+extern "C" {
+JNIEXPORT void JNICALL
+Java_org_citra_1emu_citra_MainActivity_Load(JNIEnv *env, jobject obj, jstring jpath) {
+    std::unique_ptr<EmuWindow_Android> emu_window = std::make_unique<EmuWindow_Android>();
+    Core::System &system{Core::System::GetInstance()};
+    std::string path = env->GetStringUTFChars(jpath, 0);
+    const Core::System::ResultStatus load_result{system.Load(*emu_window, path)};
+}
+}
--- src/android/app/src/main/java/org/citra_emu/citra/MainActivity.java
+++ src/android/app/src/main/java/org/citra_emu/citra/MainActivity.java
@@ -1,29 +1,59 @@
 package org.citra_emu.citra;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsProvider;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import java.io.File;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final int READ_REQUEST_CODE = 42;
 
     static{System.loadLibrary("native-lib");}
+
+    public static native void Load(String jpath);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                performFileSearch();
             }
         });
     }
 
+    public void performFileSearch() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("*/*");
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent resultData) {
+        if (resultCode == Activity.RESULT_OK) {
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                String path;
+                try {
+                    {File file = new File(uri.getPath());
+                    path = file.getCanonicalPath();}
+                    Load(path);
+                } catch (Exception e) {}
+            }
+        }
+    }
 }
